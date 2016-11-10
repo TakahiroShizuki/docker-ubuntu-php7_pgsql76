da23b63184218f64837c8afb4c184c40b13e117e
Takahiro Shizuki <shizuki@bauport.io>
2016-11-10 22:17:49 +0900

    run.bat modified.

--- run.bat
+++ run.bat
@@ -1,5 +1,2 @@
-for /f "usebackq delims=" %%a in (`PWD`) do set SHELL_PATH=%%a
-echo %SHELL_PATH%
-SET DISPLAY=192.168.1.10:0
-docker run -it --privileged --rm -e DISPLAY=%DISPLAY% -e GIT_USER_NAME="Takahiro Shizuki" -e GIT_USER_EMAIL=shizu@futuregadget.com -v /%SHELL_PATH%:/mnt/docke_dir -v /c/Users/shizuki/:/home/host_user -p 80:80 -p 5432:5432 local/ubuntu_php7_pgsql96
-
+SET HOST_IP=192.168.1.10
+docker run -it --privileged --rm -e HOST_IP=%HOST_IP% -e DISPLAY=%HOST_IP%:0 -p 80:80 -p 5432:5432 -e GIT_USER_NAME="Takahiro Shizuki" -e GIT_USER_EMAIL=shizu@futuregadget.com local/ubuntu_php7_pgsql96
